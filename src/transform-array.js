566e29f0732037604c1a63575fa62fc50d972062
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-12-14 03:03:17 +0200

    feat: solve transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -17,11 +17,48 @@ function transform(arr) {
   }
 
   return arr.flatMap((value, index, array) => {
-    if (array[index + 1] === "--discard-prev") return [];
-    if (array[index - 1] === "--double-next") return [value, value];
-    if (array[index + 1] === "--double-prev") return [value, value];
+    if (
+      array[index + 1] === "--double-prev" &&
+      array[index - 1] === "--discard-next"
+    ) {
+      return [];
+    } else if (
+      array[index + 1] === "--double-prev" &&
+      array[index - 1] === "--double-next"
+    ) {
+      return [value, value, value];
+    } else if (
+      array[index + 1] === "--discard-prev" &&
+      array[index - 1] === "--discard-next"
+    ) {
+      return [];
+    } else if (
+      array[index + 1] === "--discard-prev" &&
+      array[index - 1] === "--double-next"
+    ) {
+      return [value];
+    } else if (array[index + 1] === "--discard-prev") {
+      return [];
+    } else if (array[index + 1] === "--double-prev") {
+      return [value, value];
+    }
 
-    if (value.toString().startsWith("--d")) return [];
+    if (array[index - 1] === "--double-next") {
+      return [value, value];
+    } else if (array[index - 1] === "--discard-next") {
+      return [];
+    }
+
+    if (
+      [
+        "--discard-prev",
+        "--discard-next",
+        "--double-prev",
+        "--double-next",
+      ].includes(value.toString())
+    ) {
+      return [];
+    }
 
     return value;
   });
